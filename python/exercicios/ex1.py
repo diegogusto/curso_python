5029d5d78bd3792210e9893be0d55e343cb81d6a
diego augusto <diegoaugusto.davo@gmail.com>
2026-04-02 12:47:25 -0300

    python

---
+++ python/exercicios/ex1.py
@@ -0,0 +1,13 @@
+nome = "diego"
+sobrenome = "augusto"
+idade = 17
+nascimento = 2026 - idade
+maior_idade = idade >= 18
+altura = 1.76
+
+print('nome: ', nome)
+print("sobrenome: ", sobrenome)
+print("idade: ", idade)
+print("Ano de nascimento: ", nascimento)
+print("é maior de idade?  ", maior_idade)
+print("altura: ", altura)
---
+++ python/exercicios/ex2.py
@@ -0,0 +1,6 @@
+nome = 'luiz otavio'
+altura = 1.80
+peso = 95
+imc = peso / (altura * altura)
+
+print(f"nome: ", nome, "altura: ", altura, "peso: ", peso, "peso corporal: {imc:.1f}" )
